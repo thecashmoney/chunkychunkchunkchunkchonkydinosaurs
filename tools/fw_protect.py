a85abe8a50e59dd6089c5fb51d15a3d6722748b1
thecashmoney <elliottjang1@gmail.com>
2024-07-22 23:41:50 -0400

    Split up fw_protect into 3 functions for 3 different message types, started on start_protect

--- tools/fw_protect.py
+++ tools/fw_protect.py
@@ -6,12 +6,70 @@
 """
 Firmware Bundle-and-Protect Tool
 
+7/22/24 - Elliott Jang and Oliver Beresford
+Part 1: Start message encryption
+- start_protect(startMsg, outputMsg, version, message)
+Part 2: Data message encryption: Split into chonks first and then encrypt each chonk
+Part 3: End message encryption
+
 """
+
 import argparse
 from pwn import *
-from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
+import json
+from base64 import b64encode
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 
+
+def start_protect(startMsg, outputMsg, version, message):
+    # Load start binary from infile
+    with open(startMsg, "rb") as fp:
+        start = fp.read()
+
+    
+    # Append null-terminated message to end of firmware
+    firmware_and_message = firmware + message.encode() + b"\00"
+
+    # Pack version and size into two little-endian shorts
+    metadata = p16(version, endian='little') + p16(len(firmware), endian='little')  
+
+    # Append firmware and message to metadata
+    firmware_blob = metadata + firmware_and_message
+
+
+
+    #----------------------ENCRYPTION----------------------------------
+
+    #with open(keyfile, "rb") as key:
+    key = b""
+
+
+    header = b"header"
+
+    data = version + int.to_bytes(len(version)) + message
+    key = get_random_bytes(16)
+
+    cipher = AES.new(key, AES.MODE_GCM)
+
+    cipher.update(header)
+
+    ciphertext, tag = cipher.encrypt_and_digest(data)
+
+
+    json_k = [ 'nonce', 'header', 'ciphertext', 'tag' ]
+
+    json_v = [ b64encode(x).decode('utf-8') for x in (cipher.nonce, header, ciphertext, tag) ]
+
+    result = json.dumps(dict(zip(json_k, json_v)))
+
+    #--------------------------------------------------------------
+
+    
+    # Write firmware blob to outfile
+    with open(outfile, "wb+") as outfile:
+        outfile.write(firmware_blob)
 
 def protect_firmware(infile, outfile, version, message):
 
@@ -45,9 +103,11 @@ def protect_firmware(infile, outfile, version, message):
 
     #--------------------------------------------------------------
 
+    
     # Write firmware blob to outfile
     with open(outfile, "wb+") as outfile:
         outfile.write(firmware_blob)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Firmware Update Tool")
