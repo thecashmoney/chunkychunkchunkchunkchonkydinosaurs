a5d9253500eaad1f2fefa29ba891c01ca0c9901f
OliverSBeresford <oggynaut@icloud.com>
2024-08-03 02:22:27 -0400

    Changed and added some comments and changed a variable name in fw_update

--- tools/fw_update.py
+++ tools/fw_update.py
@@ -31,6 +31,11 @@ FRAME_BODY_LEN = 476
 
 # WORKING
 def wait_for_update():
+    """
+    Waits until the bootloader sends a U back to the python and then starts.
+    Run AFTER you start the c program (by restarting board or gdb)
+    """
+
     ser.write(b"U")
     print("Waiting for bootloader to enter update mode...")
     resp =  ser.read(1).decode()
@@ -39,6 +44,10 @@ def wait_for_update():
     print("Connection established ;)")
 
 def calc_num_frames(filedata):
+    """
+    Returns the number of frames in the protected firmware
+    """
+
     if len(filedata) % FRAME_SIZE == 0:
         frames = len(filedata) // FRAME_SIZE
         return frames
@@ -46,34 +55,39 @@ def calc_num_frames(filedata):
         print("Something is wrong with the firmware protected file.")
 
 def send_frame(ser, data, debug=False):
-    # print("SEND FRAME IS RUNNING")
+    """
+    Sends a frame including:
+        IV: 16 bytes
+        tag: 16 bytes
+        ciphertext: 480 bytes
+    """
     IV = data[0:16]
     tag = data[16:32]
     ciphertext = data[32:]
 
-    # print(len(IV))
-    # print(len(tag))
-    # frame = IV + tag + ciphertext
-    # print("IV: ", IV)
-    # print("tag: ", tag)
-    # print("ctext: ", ciphertext)
-
+    # Sending the entire frame
     frame = IV + tag + ciphertext
 
     ser.write(frame)  # Write the frame...  
 
-    #resp = read_byte()
-    #print("Bootloader responded with: ", resp)
-
-    #return resp
 
 def read_byte():
+    """
+    There are sometimes random null bytes that the bootloader sends us
+    so we just ignore them lol (we never send 0x00 from bootloader --> python)
+    """
     byte = ser.read(1)
     while byte == b'\x00':
         byte = ser.read(1)
     return byte
 
 def update(infile):
+    """
+    Initializes the update process.
+    Sends data frame by frame, waiting after decryption and again
+    after verification to send another frame
+    """
+
     num_frames = 0
 
     f = open(infile, "rb")
@@ -83,23 +97,20 @@ def update(infile):
     frames_sent = 0
 
     print("Number of frames:", num_frames)
+    # Waiting for the bootloader to respond
     wait_for_update()
 
     while frames_sent != num_frames:
+        # A frame is trying to be sent (not sent yet)
         print("Frame.")
         current_frame = data[frames_sent * 512: (frames_sent + 1) * 512]
         send_frame(ser, current_frame)
         frames_sent += 1
     
-
-        # if(response != RESP_OK):
-        #     #screaming sobbing dying
-        #     send_frame(ser, current_frame)
-            #sends back the current frame if the response is not OK
-
-        # Reads 0 if successful decryption, or RESP_RESEND if not
+        # This is whether the bootloader decrypted the frame correctly or not
         decrypt_response = read_byte()
 
+        # Script to resend the frame while the decryption didn't work, but tolerance is usually 0 so doesn't do much
         while decrypt_response!= RESP_DEC_OK:
             if decrypt_response == RESP_RESEND:
                 send_frame(ser, current_frame)
@@ -111,20 +122,19 @@ def update(infile):
                 print("Bootloader error encountered.")
                 return
 
-            
         
         # reading message type
-        message_type = read_byte()
-        if message_type == VERSION_ERROR:
+        bootloader_status = read_byte()
+        if bootloader_status == VERSION_ERROR:
             print("Version error")
             return
-        elif message_type == TYPE_ERROR:
+        elif bootloader_status == TYPE_ERROR:
             print("Problem recieving frame")
             return
-        elif message_type == STOP:
+        elif bootloader_status == STOP:
             print("Done.")
             return
-        elif message_type != RESP_OK:
+        elif bootloader_status != RESP_OK:
             print("Response error")
             return
         
